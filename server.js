71926f31475dec9af19ad7dab3988145a5680c97
corbecker <corbinbecker@protonmail.com>
2019-02-08 10:04:48 +0000

    env

--- server.js
+++ server.js
@@ -12,11 +12,11 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
-    user : 'aneagoie',
-    password : '',
-    database : 'smart-brain'
-  }
+    host: '127.0.0.1',
+    user: 'corbinbecker',
+    password: '',
+    database: 'smartbrains',
+  },
 });
 
 const app = express();
